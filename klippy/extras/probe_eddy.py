d1f8fdc7c081a5e953338e737597a628ce7bcedb
Vladimir Vukicevic <vladimir@pobox.com>
2024-12-29 19:23:20 -0800

    runs

--- klippy/extras/probe_eddy.py
+++ klippy/extras/probe_eddy.py
@@ -14,9 +14,10 @@ from klippy import Printer
 from mcu import MCU, MCU_trsync
 from stepper import MCU_stepper
 from webhooks import WebRequest
-from temperature_probe import TemperatureProbe
-from temperature_sensor import PrinterSensorGeneric
 from toolhead import ToolHead
+
+from .temperature_probe import TemperatureProbe
+from .temperature_sensor import PrinterSensorGeneric
 
 from . import ldc1612, probe, manual_probe
 
@@ -82,14 +83,18 @@ class ProbeEddyFrequencyMap:
 @final
 class ProbeEddy:
     def __init__(self, config: ConfigWrapper):
+        logging.info("Hello from ProbeEddy")
+
         self._printer: Printer = config.get_printer()
-        self._name = config.get_name()
+        self._full_name = config.get_name()
+        self._name = self._full_name.split()[-1]
 
         sensors = { "ldc1612": ldc1612.LDC1612 }
         sensor_type = config.getchoice('sensor_type', {s: s for s in sensors})
 
         self._sensor = sensors[sensor_type](config)
-        self._temp_sensor: TemperatureProbe = config.printer.load_object(config, f'temperature_probe {self.name}')
+        self._mcu = self._sensor.get_mcu()
+        self._temp_sensor: TemperatureProbe = config.printer.load_object(config, f'temperature_probe {self._name}')
 
         # these are defaults
         self.params = {
@@ -116,9 +121,14 @@ class ProbeEddy:
             "y": config.getfloat("y_offset", 0.0),
         }
 
-        self._fmap = ProbeEddyFrequencyMap(config, self.sensor, self.temp_sensor)
+        self._fmap = ProbeEddyFrequencyMap(config, self._sensor, self._temp_sensor)
 
-        gcode = self.printer.lookup_object('gcode')
+        self._endstop_wrapper = ProbeEddyEndstopWrapper(self)
+
+        self._printer.lookup_object('pins').register_chip('probe', self)
+        self._printer.add_object('probe', self)
+
+        gcode = self._printer.lookup_object('gcode')
         self.define_commands(gcode)
     
     def define_commands(self, gcode):
@@ -126,15 +136,15 @@ class ProbeEddy:
         gcode.register_command("PROBE_EDDY_CALIBRATE", self.cmd_CALIBRATE)
     
     def cmd_QUERY(self, gcmd: GCodeCommand):
-        freq = self._sensor.read_one_value()
+        freq = self._sensor.read_one_value()[1]
         height = self._fmap.raw_freqval_to_height(freq)
         gcmd.respond_info(f"Current coil value: {freq} ({height:.3f}mm)")
     
     def cmd_CALIBRATE(self, gcmd: GCodeCommand):
-        manual_probe.ManualProbeHelper(self.printer, gcmd,
+        manual_probe.ManualProbeHelper(self._printer, gcmd,
                                        lambda kin_pos: self.cmd_CALIBRATE_next(gcmd, kin_pos))
     def cmd_CALIBRATE_next(self, gcmd: GCodeCommand, kin_pos: float):
-        toolhead: ToolHead = self.printer.lookup_object('toolhead')
+        toolhead: ToolHead = self._printer.lookup_object('toolhead')
         toolhead_kin = toolhead.get_kinematics()
 
         # we're going to set the kinematic position to kin_height to make
@@ -183,7 +193,7 @@ class ProbeEddy:
         first_sample_time = None
         last_sample_time = None
 
-        with sampler := ProbeEddySampler(self.printer, self.sensor):
+        with ProbeEddySampler(self._printer, self._sensor) as sampler:
             # move down in steps, taking samples
             for z in np.arange(cal_z_max, 0, -self.params['calibration_step']):
                 # hop up, then move downward to z to reduce backlash
@@ -221,10 +231,71 @@ class ProbeEddy:
         heights = [toolhead_kin_z(at=s[0]) for s in samples]
 
         # and build a map
-        mapping = ProbeEddyFrequencyMap(self.printer, self.sensor, self.temp_sensor)
+        mapping = ProbeEddyFrequencyMap(self._printer, self._sensor, self._temp_sensor)
         r2_fth, r2_htf = mapping.calibrate_from_values(freqs, heights)
 
         gcmd.respond_info(f"Calibration complete, R^2: freq-to-height={r2_fth:.3f}, height-to-freq={r2_htf:.3f}")
+
+    # Virtual endstop
+
+    def setup_pin(self, pin_type, pin_params):
+        if pin_type != "endstop" or pin_params["pin"] != "z_virtual_endstop":
+            raise pins.error("Probe virtual endstop only useful as endstop pin")
+        if pin_params["invert"] or pin_params["pullup"]:
+            raise pins.error("Can not pullup/invert probe virtual endstop")
+        return self._endstop_wrapper
+
+    # Probe interface
+
+    def multi_probe_begin(self):
+        pass
+
+    def multi_probe_end(self):
+        pass
+
+    def get_offsets(self):
+        return self.offset["x"], self.offset["y"], self.params['home_trigger_z']
+
+    def get_lift_speed(self, gcmd: Optional[GCodeCommand] = None):
+        if gcmd is not None:
+            return gcmd.get_float("LIFT_SPEED", self.lift_speed, above=0.0)
+        return self.params['lift_speed']
+
+    def get_samples(self, gcmd: Optional[GCodeCommand] = None):
+        if gcmd is not None:
+            return gcmd.get_int("SAMPLES", self.samples_config["samples"], minval=1)
+        return 1 # self.samples_config["samples"]
+
+    def get_sample_retract_dist(self, gcmd: Optional[GCodeCommand] = None):
+        if gcmd is not None:
+            return gcmd.get_float(
+                "SAMPLE_RETRACT_DIST", self.samples_config["retract_dist"], above=0.0
+            )
+        return 0 # self.samples_config["retract_dist"]
+
+    def get_samples_tolerance(self, gcmd: Optional[GCodeCommand] = None):
+        if gcmd is not None:
+            return gcmd.get_float(
+                "SAMPLES_TOLERANCE", self.samples_config["tolerance"], minval=0.0
+            )
+        return 0.001
+
+    def get_samples_tolerance_retries(self, gcmd: Optional[GCodeCommand] = None):
+        if gcmd is not None:
+            return gcmd.get_int(
+                "SAMPLES_TOLERANCE_RETRIES",
+                self.samples_config["tolerance_retries"],
+                minval=0,
+            )
+        return 5
+
+    def get_samples_result(self, gcmd: Optional[GCodeCommand] = None):
+        if gcmd is not None:
+            return gcmd.get("SAMPLES_RESULT", self.samples_config["result"])
+        return self.samples_config["result"]
+
+    def run_probe(self, gcmd: GCodeCommand):
+        raise gcmd.error("can't run_probe")
 
 
 # Tool to gather samples and convert them to probe positions
@@ -264,3 +335,33 @@ class ProbeEddySampler:
     
     def get_error_count(self):
         return self._errors
+
+@final
+class ProbeEddyEndstopWrapper:
+    def __init__(self, eddy: ProbeEddy):
+        self.eddy = eddy
+        self._mcu = eddy._mcu
+
+    def get_mcu(self):
+        return self._mcu
+
+    def add_stepper(self, stepper: MCU_stepper):
+        pass
+
+    def get_steppers(self):
+        return []
+
+    def home_start(self, print_time, sample_time, sample_count, rest_time, triggered=True):
+        raise self.eddy._printer.error("Can't home yet")
+
+    def home_wait(self, home_end_time):
+        raise self.eddy._printer.error("Can't home yet")
+
+    def query_endstop(self, print_time):
+        return 1
+
+    def get_position_endstop(self):
+        return self.eddy.params['home_trigger_z']
+
+def load_config_prefix(config: ConfigWrapper):
+    return ProbeEddy(config)
